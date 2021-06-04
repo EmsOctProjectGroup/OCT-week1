2897d41a48e409f8481b942d8b48dff04324559a
Nam Tran <83447048+mentatss@users.noreply.github.com>
2021-06-04 12:04:59 +0200

    Update Rohdaten.m

--- Rohdaten.m
+++ Rohdaten.m
@@ -2,9 +2,43 @@ FileName = 'phantom1_2_2raw.dat';
 FolderName = 'C:/Users/haina/Downloads/';
 File = fullfile(FolderName, FileName);
 fileID = fopen(File);
-A=fread(fileID,[1024,410001],'uint16');
-%%
+A=fread(fileID,[1024,inf],'uint16');
 A=A*540;
 %%
-colormap gray;
-image(A(:,1000));
+B_off=A-Offset;
+%%
+mittel=mean(B_off,2);
+%%
+% mittel_rep = repmat(mittel,1,410001);
+%%
+DC =  B_off - repmat(mittel,1,410001);
+%%
+figure(1);
+plot(DC(:,1356));
+%% Interpolation
+for count = 1:410001
+    interpolation(:,count)=interp1(1:1024,DC(:,count),Chirp);
+end
+%%
+plot(interpolation(:,1));
+%% Hann
+hw = hann(1024);
+multwithhw = interpolation.*hw;
+multwithhw(1,:)=0;
+figure('name','Hann-Fenster')
+%%
+plot(multwithhw(:,1256));
+%% Fourier
+for count = 1:410001
+    fourier(:,count) = (abs(fft(multwithhw(:,count))));
+    compr(:,count) = 20*log10(fourier(:,count));
+end
+%%
+compr_mult=fix(compr*2.46);
+%%
+figure(1);
+colormap gray(350);
+image(compr_mult(1:512,:));
+figure(2);
+colormap gray(350);
+image(mscancut);
