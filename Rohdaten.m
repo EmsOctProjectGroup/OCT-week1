bd283c258252fbee8fa1f06ddf46aaa0c6a1fe96
Nam Tran <83447048+mentatss@users.noreply.github.com>
2021-06-10 16:17:24 +0200

    Remove all for loop

--- Rohdaten.m
+++ Rohdaten.m
@@ -12,30 +12,30 @@ B_off=A-Offset;
 DC =  B_off - mean(B_off,2);
 
 %% Interpolation
-% interpolation=bsxfun(@interp1, DC, Chirp);
-for count = 1:410001
-    interpolation(:,count)=interp1(1:1024,DC(:,count),Chirp);
-end
+interpolation=interp1(DC,Chirp);
+
 %%
+subplot(2,1,1);
+plot(DC(:,1));
+subplot(2,1,2);
 plot(interpolation(:,1));
+
 %% Hann
 hw = hann(1024);
 multwithhw = interpolation.*hw;
 multwithhw(1,:)=0;
-figure('name','Hann-Fenster')
 %%
-plot(multwithhw(:,1256));
+plot(multwithhw(:,187));
 %% Fourier
-for count = 1:410001
-    fourier(:,count) = (abs(fft(multwithhw(:,count))));
-    compr(:,count) = 20*log10(fourier(:,count));
-end
+fourier=abs(fft(multwithhw));
+compr=20*log10(fourier);
+
 %%
 compr_mult=fix(compr*2.46);
 %%
 figure(1);
 colormap gray(350);
 image(compr_mult(1:512,:));
-figure(2);
-colormap gray(350);
-image(mscancut);
+% figure(2);
+% colormap gray(350);
+% image(mscancut);
